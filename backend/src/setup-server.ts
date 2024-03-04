789b61ebcec6fdb902873c7e0ed8342027d9fe6e
cisu <georgrekis@gmail.com>
2024-03-04 16:44:39 +0200

    install more libraries and work with security and standar middlewares

--- backend/src/setup-server.ts
+++ backend/src/setup-server.ts
@@ -1,4 +1,12 @@
 import { json, urlencoded, Response, Request, NextFunction } from "express";
+import cors from "cors";
+import helmet from "helmet";
+import hpp from "hpp";
+import compression from "compression";
+import cookieSession from "cookie-session";
+import HTTP_STATUS from "http-status-codes";
+
+import "express-async-errors";
 
 // import types
 import { type Application } from "express";
@@ -19,9 +27,36 @@ export class SocialiserServer {
     this.startServer(this.app);
   }
 
-  private securityMiddleware(app: Application): void {}
+  private securityMiddleware(app: Application): void {
+    app.use(
+      cookieSession({
+        name: "session",
+        keys: ["test1", "test2"],
+        maxAge: 24 * 7 * 3600,
+        secure: false,
+      })
+    );
+    app.use(hpp());
+    app.use(helmet());
+    app.use(
+      cors({
+        origin: "*",
+        credentials: true,
+        optionsSuccessStatus: 200,
+        methods: ["GETS", "POST", "PUT", "DELETE", "OPTIONS"],
+      })
+    );
+  }
 
-  private standardMiddleware(app: Application): void {}
+  private standardMiddleware(app: Application): void {
+    // helps to compress our requests and response.
+    app.use(compression());
+    // set a limit to request
+    // if exceeds 50 megabytes it's going to throw an error.
+    app.use(json({ limit: "50mb" }));
+
+    app.use(urlencoded({ extended: true, limit: "50mb" }));
+  }
 
   private routesMiddleware(app: Application): void {}
 
